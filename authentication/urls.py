d8f213f83b9b477f27abbd076fdaae86dc8142a0
Eduarda Khvan <edyakhvan@gmail.com>
2026-05-10 03:37:58 +0500

    first commit

---
+++ authentication/urls.py
@@ -0,0 +1,12 @@
+from .views import RegistrationView, UsernameValidationView, EmailValidationView
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+
+urlpatterns=[
+    path('register', RegistrationView.as_view(), name="register"),
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()),
+        name="validate-email"),
+        path('validate-email', csrf_exempt(EmailValidationView.as_view()), 
+             name='validate_email')
+]
+
