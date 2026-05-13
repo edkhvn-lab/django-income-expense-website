7555dcc598a9f4133027b3799733346e82d53a9c
Eduarda Khvan <edyakhvan@gmail.com>
2026-05-13 22:30:54 +0500

    remove pycache and cleanup

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import JsonResponse
 from django.contrib.auth.models import User
@@ -6,6 +6,18 @@ import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email
+from django.contrib import messages
+from django.core.mail import EmailMessage
+from django.urls import reverse
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.sites.shortcuts import get_current_site
+from .utils import token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.contrib import auth
+
+
 
 
 class EmailValidationView(View):
@@ -31,7 +43,131 @@ class UsernameValidationView(View):
         return JsonResponse({'username_valid': True})
 
 
-
 class RegistrationView(View):
     def get(self, request):
-        return render(request,'authentication/register.html')
+        return render(request, 'authentication/register.html')
+    
+    def post(self, request):
+
+        username = request.POST['username']
+        user_email = request.POST['email']
+        password = request.POST['password']
+
+        context = {
+            'fieldValues': request.POST
+        }
+
+        if not User.objects.filter(username=username).exists():
+            if not User.objects.filter(email=email).exists():
+                if len(password) < 6:
+                    messages.error(request, 'Password too short')
+                    return render(request, 'authentication/register.html', context)
+                user = User.objects.create_user(username=username, email=email)
+                user.set_password(password)
+                user.is_active = False
+                user.save()
+
+
+                # page_to_view
+
+                
+                uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+                domain = get_current_site(request).domain
+                link = reverse('activate',kwargs={'uidb64': uidb64, 'token': token_generator.make_token(user)})
+                activate_url = f'http://{domain}{link}'
+
+               
+
+                email_subject = 'Verify your account'
+
+                email_body = f"""
+                Hi {user.username},
+
+                Please use the link below to verify your account:
+
+                {activate_url}
+                """
+                email= EmailMessage(
+                    email_subject,
+                    email_body,
+                    'edyakhvan@gmail.com',
+                    [user_email],
+                )
+
+                email.send(fail_silently=False)
+                messages.success(request, 'Account successfully created')
+                return render(request, 'authentication/register.html')
+
+        return render(request, 'authentication/register.html')
+    
+
+
+
+class VerificationView(View):
+
+    def get(self, request, uidb64, token):
+        try:
+            user_id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=user_id)
+
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        # если пользователь существует и токен валиден
+        if user is not None and token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+
+            messages.success(request, 'Account activated successfully')
+            return redirect('login')
+
+        # если пользователь есть, но уже активирован
+        elif user is not None and user.is_active:
+            messages.info(request, 'Account already activated')
+            return redirect('login')
+
+        # если токен неправильный/просрочен
+        else:
+            messages.error(request, 'Activation link is invalid or expired')
+            return redirect('login')
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'authentication/login.html')
+    
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if username and password:
+            user=auth.authenticate(username=username, password=password)
+
+            if user:
+                if user.is_active:
+                    auth.login(request,user)
+                    messages.success(request, f'Welcome, {user.username} you are now logged in')
+                    return redirect('expenses')
+
+                messages.error(
+                    request, "Account is not active, please check your email") 
+                return render(request, 'authentication/login.html')
+            messages.error(
+                request, "Invalid credentials, try again") 
+            return render(request, 'authentication/login.html')
+
+       
+        messages.error(
+                request, "Please fill all fields") 
+        return render(request, 'authentication/login.html')
+    
+
+class LogoutView(View):
+    def post(self, request):
+        auth.logout(request)
+        messages.success(request, "You have been logged out")
+        return redirect('login')
+
+
+                
+
--- expensewebsite/settings.py
+++ expensewebsite/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'expenses'
+    'expenses',
+    'userpreferences'
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,16 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
+MESSAGE_TAGS={
+    messages.ERROR : 'danger'
+}
+
+
+# email stuff
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
+EMAIL_USE_TLS = True
